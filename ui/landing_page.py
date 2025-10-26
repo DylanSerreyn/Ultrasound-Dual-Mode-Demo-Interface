8848209e1955e56f142f5f4461b33c46aa7aec10
Dylan Serreyn <dsserreyn@wpi.edu>
2025-10-26 15:45:49 -0400

    added safe exit

--- ui/landing_page.py
+++ ui/landing_page.py
@@ -17,10 +17,31 @@ class LandingPage(QWidget):
         tracker_btn.setFixedHeight(48)
         tracker_btn.clicked.connect(on_tracker_clicked)
 
+#-------------------------------------------------
+# haha
+#-------------------------------------------------
+        self.dont_click_btn = QPushButton("Don't click")
+        self.dont_click_btn.setFixedHeight(40)
+        self.dont_click_btn.clicked.connect(self._dont_click_handler)
+        self._dont_click_stage = 0
+        
+
         layout = QVBoxLayout()
         layout.addWidget(title)
         layout.addSpacing(12)
         layout.addWidget(rps_btn)
         layout.addWidget(tracker_btn)
+        layout.addWidget(self.dont_click_btn) # haha
         layout.addStretch()
         self.setLayout(layout)
+
+    def _dont_click_handler(self):
+        """Cycles through funny warnings, then exits the app."""
+        self._dont_click_stage += 1
+        if self._dont_click_stage == 1:
+            self.dont_click_btn.setText("I said DON'T click")
+        elif self._dont_click_stage == 2:
+            self.dont_click_btn.setText(":( ")
+        elif self._dont_click_stage >= 3:
+            from PySide6.QtWidgets import QApplication
+            QApplication.quit()
